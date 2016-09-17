92f30c07dc73d4832de48663bf0a93688ff6c7ae
Quang Thịnh <quangthinh.nguyen91@gmail.com>
2016-09-17 08:18:50 +0000

    add jquery

---
+++ documents/Web/Javascript/Jquery/Request.js
@@ -0,0 +1,24 @@
+//Submit form with
+$('#login_form').submit(function(event) {
+    var data = $(this).serialize();
+    $.ajax({
+        type: 'POST',
+        url: MAIN_URL + 'users',
+        data: data,
+        dataType: 'JSON',
+        success: function(json){
+            if(json.result){
+                //Using Toastr Notification
+                toastr.success(json.msg);
+                $(this).closest('form').find("input[type=text], textarea").val("");
+            }
+            else{
+                toastr.error(json.msg);
+            }
+        },
+        error: function(xhr, textStatus, errorThrown){
+            console.log('Không thể kết nối');
+        }
+    });
+    return false;
+});
